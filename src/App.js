1ce57f64c0c514093bf175b7ca1bf7d656b599b9
Gabriel <gabriel.sgaspar@yahoo.com.br>
2019-12-25 19:31:35 -0300

    Rota para o componente Orders criada

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from './containers/Layout/Layout';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import BurgerBuilder from './containers/BurguerBuilder/BurguerBuilder';
 import Checkout from './containers/Checkout/checkout';
+import Orders from './containers/Orders/Orders';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Layout> 
           <Switch>
+            <Route path="/orders" component={Orders} />
             <Route path="/checkout" component={Checkout} />
             <Route path="/" component={BurgerBuilder} />
           </Switch>

8b2d0aa692bab811ea807ab7ecc97f9d6b8a4124
Gabriel <gabriel.sgaspar@yahoo.com.br>
2019-12-25 21:01:51 -0300

    Componente Order está pronto, comunicação com o FireBase também

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,23 +1,48 @@
 import React, {Component} from 'react';
 
+import withErrorHandler from '../../components/hoc/withErrorHandler/withErrorHandler';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
 
 import classes from './Orders.module.css';
 
 class Orders extends Component {
     state ={
+        loading: true,
+        orders:[]
+    }
 
+    componentDidMount(){
+        axios.get('/orders.json')
+            .then(resp => {
+                const fetchedOrders = [];
+                for(let key in resp.data){
+                    fetchedOrders.push({
+                        ...resp.data[key],
+                        id: key
+                    });
+                }
+                this.setState({loading:false, orders: fetchedOrders});
+            })
+            .catch(err=>{
+                this.setState({loading:false});
+            })
     }
 
     render(){
         return(
             <div className={classes.Orders}>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => {
+                    return (
+                        <Order  key = {order.id}
+                                ingredients={order.ingredients}
+                                price={order.totalPrice}/>
+                    );
+                })}
             </div>
         );
     }
 
 }
 
-export default Orders;
+export default withErrorHandler(Orders,axios);

54732e63bd88e9d820f79e63928aa4c1047e008b
Gabriel <gabriel.sgaspar@yahoo.com.br>
2019-12-27 20:23:43 -0300

    Componente Input criado e estilizado

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import classes from './Input.module.css'
+
+const input = (props) => {
+
+    let inputElement = null;
+
+    switch(props.inputType){
+        case('input'):
+            inputElement = <input className={classes.inputElement} {...props} />;
+            break;
+        case('textarea'):
+            inputElement = <textarea className={classes.inputElement} {...props} />;
+            break;
+        default:
+            inputElement = <input className={classes.inputElement} {...props} />;
+    } 
+
+    return(
+        <div className={classes.Input}>
+            <label className={classes.label}>{props.label}</label>
+            {inputElement}
+        </div>
+    );
+}
+
+export default input;

e605afb9a4ea179792fb957236729a5efe9f31dd
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-05-15 20:51:42 -0300

    refatora order reducer com a função updateObject

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -8,6 +8,8 @@ import {
     FETCH_ORDERS_FAIL
 } from '../actions/actionTypes';
 
+import {updateObject} from './utility';
+
 const initialState = {
     orders: [],
     loading: false,
@@ -16,48 +18,36 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch(action.type){
+        
         case PURCHASE_INIT:
-            return {
-                ...state,
-                purchased: false
-            }
+            return updateObject(state, {purchased: false});
+
         case PURCHASE_BURGER_START:
-            return{
-               ...state,
-               loading: true 
-            };
+            return updateObject(state, {loading: true });
+
         case PURCHASE_BURGER_SUCCESS:
-            const newOrder = {
-                ...action.orderData,
-                id: action.orderId,
-            }    
-            return{
-                ...state,
+            const newOrder = updateObject(action.orderData, {id: action.orderId});
+            return updateObject(state, {
                 loading: false,
                 purchased: true,
                 orders: state.orders.concat(newOrder)
-            };
+            });
+        
         case PURCHASE_BURGER_FAIL:
-            return {
-                ...state,
-                loading: false
-            };          
+            return updateObject(state, {loading: false});
+
         case FETCH_ORDERS_INIT:
-            return{
-                ...state,
-                loading: true
-            };
+            return updateObject(state, {loading: true});
+            
         case FETCH_ORDERS_SUCCESS:
-            return{
-                ...state,
+            return updateObject(state, {
                 orders: action.orders,
                 loading: false
-            };
+            });
+
         case FETCH_ORDERS_FAIL:
-            return {
-                ...state,
-                loading: false,
-            };
+            return updateObject(state, {loading: false});
+
         default:
             return state;
     }

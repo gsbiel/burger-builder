12f6d550b900515cbfffd00c23c0fe6e59b9bb32
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-05-17 00:03:23 -0300

    implementa cadastro de usuário junto ao Firebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -4,6 +4,14 @@ import {
     AUTH_SUCCESS
 } from './actionTypes';
 
+import {
+    getApiKey
+} from '../../Firebase';
+
+import axios from 'axios';
+
+const AUTH_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${getApiKey()}`;
+
 export const authStart = () => {
     return {
         type: AUTH_START
@@ -26,6 +34,20 @@ export const authFail = (error) => {
 
 export const auth = (email, password) => {
     return (dispatch) => {
-        dispatch(authStart);
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        axios.post(AUTH_URL, authData)
+            .then(resp => {
+                console.log(resp.data);
+                dispatch(authSuccess(resp.data));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail());
+            });
     };
 };
